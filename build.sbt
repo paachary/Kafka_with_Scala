d2814a8b7176ad2d2bc951de4f5623035ab2d6ad
paachary <prashant_acharya74@yahoo.com>
2020-04-17 17:43:47 +0530

    Added documentation.

--- build.sbt
+++ build.sbt
@@ -2,7 +2,7 @@ name := "Kafka_with_Scala"
 
 version := "0.1"
 
-scalaVersion := "2.13.1"
+scalaVersion := "2.11.12"
 
 libraryDependencies += "org.apache.kafka" % "kafka-clients" % "2.4.1" exclude("log4j", "log4j") exclude("org.slf4j","slf4j-log4j12")
 
--- src/main/scala/kafka/Consumer.scala
+++ src/main/scala/kafka/Consumer.scala
@@ -2,17 +2,27 @@ package kafka
 
 import java.util
 
-import scala.collection.JavaConverters._
 import org.apache.kafka.clients.consumer._
-import org.slf4j.{Logger, LoggerFactory}
-import sun.misc.{Signal, SignalHandler}
+import org.slf4j.LoggerFactory
 
-import scala.concurrent.duration.Duration
+import scala.collection.JavaConverters._
 
+/**
+ * This class is responsible for
+ * Consuming data from Kafka via a kafka topic and set of
+ * required Kafka Configuration properties
+ *
+ */
 class Consumer {
 
   val consumerGroupId = "first_application_group"
 
+  /**
+   * This function creates a kafka consumer instance
+   * and with required set of kafka configuration properties,
+   * reads data from kafka topic.
+   * @param topic : A topic name of string datatype
+   */
   def readFromKafka( topic : String) : Unit = {
 
     val log = LoggerFactory.getLogger(classOf[Consumer])
@@ -45,8 +55,12 @@ class Consumer {
   }
 }
 
+/**
+ * A companion object for the Kafka consumer class which
+ * initiates the consumer
+ */
 object Consumer extends App {
 
   val consumer = new Consumer
-  consumer.readFromKafka("tweet_topic")
+  consumer.readFromKafka("tweet_new_topic")
 }
